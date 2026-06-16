03cf5a0738be440984b132dd43b49c073d135d86
nicollas-faria <176961733+nicollas-faria@users.noreply.github.com>
2026-06-16 00:07:22 -0300

    Aprendendo a usar loops corretamente.

---
+++ Estudos/DesafiosLoops.py
@@ -0,0 +1,33 @@
+# exercicios comentados para nao quebrar o conteudo.
+
+# desafio 1
+
+#2numero = 1
+#while numero < 11:
+#    print(numero)
+#    numero += 1
+
+# desafio 2
+
+#num = int(input('Digite um numero: '))
+#for i in range(1, 11):
+#    print(f'{num} x {i} = {num * i}')
+
+# desafio 3
+
+#frase = input('Digite uma frase: ')
+#vogais = 'aeiou'.lower()
+#contador_vogais = 0
+#for letra in frase:
+#    if letra in vogais:
+#        contador_vogais += 1
+#print(f'A frase {frase} tem {contador_vogais} vogais.')
+
+# desafio 4
+
+#numero = 0
+#for n in range(1, 101):
+#    for i in range(1, 11):
+#        mult = n * i
+#        print(f'{n} x {i} = {mult}')
+#    print('\n')
---
+++ Estudos/Loops.py
@@ -0,0 +1,11 @@
+#soma = 0
+#for n in range(1, 11):
+#    soma += 1
+
+#print(f'A soma total é {soma}')
+
+# loops com dicionarios:
+dados = {'nome':'Nicollas', 'idade': 18, 'Cidade': 'São Paulo'}
+
+for c, v in dados.items():
+    print(f'{c} = {v}')
